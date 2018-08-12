8d3b71a055097c6e1945bd51933aa9c0102e0613
Troy Leach <troyleach29@gmail.com>
2018-08-12 15:14:43 -0400

    user sign up with flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
-  get 'users/create'
-
   root 'home#index'
+  resources :users
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,11 +9,34 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    describe "create a new user" do
+      it "positive result, user is created" do
+        params = { user: {
+          username: 'username',
+          email: 'email@test.com',
+          password: 'password'
+        } }
+
+        get :create, params: params
+        users = User.all
+        expect(users.length).to eq(1)
+        expect(subject.request.flash[:success]).to eq('Successfully signed up')
+        expect(response).to redirect_to( root_path )
+      end
+
+      it "negative result, password not long enough" do
+        params = { user: {
+          username: 'username',
+          email: 'email@test.com',
+          password: 'pass'
+        } }
+
+        get :create, params: params
+        users = User.all
+        expect(users.length).to eq(0)
+        expect(subject.request.flash[:warning]).to eq('Form is invalid')
+      end
     end
   end
-
 end
